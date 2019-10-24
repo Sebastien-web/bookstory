58572f340b61ff1395d98c782536fc67a657a6d1
Eikyuu <v.duguet@laposte.net>
2019-10-24 09:07:52 +0200

    add feature story page

---
+++ src/Controller/StoryController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aurelwcs
+ * Date: 08/04/19
+ * Time: 18:40
+ */
+
+namespace App\Controller;
+
+class StoryController extends AbstractController
+{
+
+    /**
+     * Display home page
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function story()
+    {
+        return $this->twig->render('Story/story.html.twig');
+    }
+}
