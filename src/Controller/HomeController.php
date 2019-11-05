00c0028a0ce5fc50548b779d269b825c6d10c100
Sheitak <savingunited@gmx.fr>
2019-11-05 18:08:15 +0100

    layout update and story update

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -19,8 +19,30 @@ class HomeController extends AbstractController
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\SyntaxError
      */
-    public function index()
+    public function index($parameter)
     {
-        return $this->twig->render('Home/index.html.twig');
+        return $this->twig->render('Home/index.html.twig', [
+            'gender' => $parameter
+        ]);
+    }
+
+    public function contact()
+    {
+        return $this->twig->render('Home/contact.html.twig');
+    }
+
+    public function apropos()
+    {
+        return $this->twig->render('Home/apropos.html.twig');
+    }
+
+    public function legendes()
+    {
+        return $this->twig->render('Home/legendes.html.twig');
+    }
+
+    public function conditions()
+    {
+        return $this->twig->render('Home/conditions.html.twig');
     }
 }
--- src/Controller/StoryController.php
+++ src/Controller/StoryController.php
@@ -19,8 +19,34 @@ class StoryController extends AbstractController
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\SyntaxError
      */
-    public function story()
+
+    public function stage1()
     {
-        return $this->twig->render('Story/story.html.twig');
+        return $this->twig->render('Story/stage1.html.twig');
+    }
+
+    public function stage2foret()
+    {
+        return $this->twig->render('Story/stage2foret.html.twig');
+    }
+
+    public function stage2montagne()
+    {
+        return $this->twig->render('Story/stage2montagne.html.twig');
+    }
+
+    public function stage3fuite()
+    {
+        return $this->twig->render('Story/stage3fuite.html.twig');
+    }
+
+    public function stage3attaque()
+    {
+        return $this->twig->render('Story/stage3attaque.html.twig');
+    }
+
+    public function stage4sbires()
+    {
+        return $this->twig->render('Story/stage4sbires.html.twig');
     }
 }
