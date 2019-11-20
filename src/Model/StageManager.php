4b00b53da906d16d965bf9e1db0927a618476b52
Sebastien Comberousse <scomberousse@gmail.com>
2019-11-20 14:17:19 +0100

    indentation corrected in StageManager

--- src/Model/StageManager.php
+++ src/Model/StageManager.php
@@ -7,13 +7,13 @@ class StageManager extends AbstractManager
 /**
  *
  */
-public function selectOneByStage(string $stage)
-{
-    // prepared request
-    $statement = $this->pdo->prepare("SELECT * FROM $this->table WHERE `stage`=:stage");
-    $statement->bindValue('stage', $stage);
-    $statement->execute();
+    public function selectOneByStage(string $stage)
+    {
+        // prepared request
+        $statement = $this->pdo->prepare("SELECT * FROM $this->table WHERE `stage`=:stage");
+        $statement->bindValue('stage', $stage);
+        $statement->execute();
 
-    return $statement->fetch();
-}
+        return $statement->fetch();
+    }
 }
